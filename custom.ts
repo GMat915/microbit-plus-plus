dc412c39cd448136e3680c079f4a06769ebbd628
GMat915 <97854407+GMat915@users.noreply.github.com>
2022-01-23 14:31:11 -0500

    added "7 seconds game" block

--- custom.ts
+++ custom.ts
@@ -156,4 +156,25 @@ namespace PlusPlus {
         })
 
     }
+
+    /**
+     * 7 Seconds Game!
+     */
+    //% block="7 seconds"
+    //% group=Games
+    export function sevenseconds(): void {
+        let start = 0;
+        let elapsed = 0;
+        let score = 0;
+        input.onButtonPressed(Button.A, function () {
+            start = input.runningTime();
+            basic.showIcon(IconNames.Chessboard);
+        })
+
+        input.onButtonPressed(Button.B, function () {
+            elapsed = input.runningTime() - start
+            score = Math.abs(elapsed - 7000)
+            basic.showNumber(score)
+        })
+    }
 }
